02c5562621bfcdc534810e6b49decc91cf8eb4ed
Rakeem <rakeemshaikh4545@gmail.com>
2023-02-04 07:09:12 +0530

    learning hashing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -18,9 +18,6 @@ const userSchema = new mongoose.Schema({
     email:String,
     password:String
 });
-
-const secrets = process.env.SECRET;
-userSchema.plugin(encrypt,{secret:secrets, encryptedFields:["password"]});
 
 const User = mongoose.model("user",userSchema);
 
@@ -39,7 +36,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const user = new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
 
     user.save(function(err){
@@ -54,7 +51,7 @@ app.post("/register",function(req,res){
 app.post("/login",function(req,res){
     User.findOne({email:req.body.username},function(err,user){
         if(user){
-            if(user.password == req.body.password){
+            if(user.password == md5(req.body.password)){
                 res.render("secrets");
             }else{
                 console.log("the password is incorrect");
